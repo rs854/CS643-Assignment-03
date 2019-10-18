5a2ddb89041dea7182b921e3be8e11581ab7974c
robert <rs854@njit.edu>
2019-10-18 00:12:37 +0000

    code cleanup

--- WordCount.java
+++ WordCount.java
@@ -10,36 +10,37 @@ import org.apache.hadoop.mapreduce.lib.output.*;
 
 public class WordCount {
 
-  public static class TokenizerMapper
-       extends Mapper<Object, Text, Text, IntWritable>{
+  public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
 
     private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
     private Text state = new Text();
 
-    public void map(Object key, Text value, Context context
-                    ) throws IOException, InterruptedException {
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException 
+    {
       String fileName = ((FileSplit) context.getInputSplit()).getPath().getName();
       state.set(fileName);
       StringTokenizer itr = new StringTokenizer(value.toString());
+
       while (itr.hasMoreTokens()) {
         word.set(itr.nextToken().toLowerCase().trim());
-	String wordStr = word.toString();
-	if (wordStr.equals("education") || wordStr.equals("politics") || wordStr.equals("sports") || wordStr.equals("agriculture"))
-	{
-	  context.write(state, one);
-	}
+
+        String wordStr = word.toString();
+
+        if (wordStr.equals("education") || wordStr.equals("politics") || wordStr.equals("sports") || wordStr.equals("agriculture")) 
+        {
+          context.write(state, one);
+        }
       }
     }
   }
 
-  public static class IntSumReducer
-       extends Reducer<Text,IntWritable,Text,IntWritable> {
+  public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> 
+  {
     private IntWritable result = new IntWritable();
 
-    public void reduce(Text key, Iterable<IntWritable> values,
-                       Context context
-                       ) throws IOException, InterruptedException {
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException 
+    {
       int sum = 0;
       for (IntWritable val : values) {
         sum += val.get();
