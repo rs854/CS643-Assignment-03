af9fdfd249256f08817911104ecd2b08233cca18
robert <rs854@njit.edu>
2019-10-18 01:53:58 +0000

    counts words separately for each state

--- WordCount.java
+++ WordCount.java
@@ -10,14 +10,12 @@ import org.apache.hadoop.mapreduce.lib.output.*;
 
 public class WordCount {
 
-  public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+  public static class TokenizerMapper extends Mapper<Object, Text, Text, Text> {
 
-    private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
     private Text state = new Text();
 
-    public void map(Object key, Text value, Context context) throws IOException, InterruptedException 
-    {
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
       String fileName = ((FileSplit) context.getInputSplit()).getPath().getName();
       state.set(fileName);
       StringTokenizer itr = new StringTokenizer(value.toString());
@@ -27,25 +25,49 @@ public class WordCount {
 
         String wordStr = word.toString();
 
-        if (wordStr.equals("education") || wordStr.equals("politics") || wordStr.equals("sports") || wordStr.equals("agriculture")) 
-        {
-          context.write(state, one);
+        if (wordStr.equals("education") || wordStr.equals("politics") || wordStr.equals("sports")
+            || wordStr.equals("agriculture")) {
+          context.write(state, word);
         }
       }
     }
   }
 
-  public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> 
-  {
-    private IntWritable result = new IntWritable();
+  public static class IntSumReducer extends Reducer<Text, Text, Text, Text> {
+    private Text result = new Text();
 
-    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException 
-    {
+    public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+      int educationSum = 0;
+      int politicsSum = 0;
+      int sportsSum = 0;
+      int agricultureSum = 0;
+      String valStr = "";
+      String debug = "";
       int sum = 0;
-      for (IntWritable val : values) {
-        sum += val.get();
+
+      for (Text val : values) {
+        valStr = val.toString();
+        if (valStr.equals("education"))
+        {
+          educationSum++;
+        }
+        if (valStr.equals("politics"))
+        {
+          politicsSum++;
+        }
+        if (valStr.equals("sports"))
+        {
+          sportsSum++;
+        }
+        if (valStr.equals("agriculture"))
+        {
+          agricultureSum++;
+        }
       }
-      result.set(sum);
+
+      //result.set(String.format("%d - %s", sum, debug));
+      result.set(String.format("%d %d %d %d", educationSum, politicsSum, sportsSum, agricultureSum));
+      //result.set(debug);
       context.write(key, result);
     }
   }
@@ -55,10 +77,10 @@ public class WordCount {
     Job job = Job.getInstance(conf, "word count");
     job.setJarByClass(WordCount.class);
     job.setMapperClass(TokenizerMapper.class);
-    job.setCombinerClass(IntSumReducer.class);
+    //job.setCombinerClass(IntSumReducer.class);
     job.setReducerClass(IntSumReducer.class);
     job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
+    job.setOutputValueClass(Text.class);
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
